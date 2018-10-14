a3c2ae16e1d59e1f7d3e75812d623ff632781b65
Simon Lichtnecker <cormensratio@posteo.de>
2018-10-14 19:38:07 +0200

    Add new switch/if test cases
    Adds new small ground truth test cases for switch and nested if handling.
    
    related se-passau/VaRA#247

---
+++ examples/if.cpp
@@ -0,0 +1,51 @@
+#include "options.h"
+
+bool level_1 = false;
+bool level_2 = false;
+bool level_3 = false;
+bool level_4 = false;
+
+long int sleeping_time_seconds = TIME_DURATION_SECONDS;
+long int sleeping_time_nanoseconds = TIME_DURATION_NANOSECONDS;
+
+void level_check() { // Can be used to ensure that every activated level can be
+                     // reached
+  if (level_2) {
+    level_1 = true;
+  }
+  if (level_3) {
+    level_2 = true;
+    level_1 = true;
+  }
+  if (level_4) {
+    level_3 = true;
+    level_2 = true;
+    level_1 = true;
+  }
+}
+
+int main() {
+
+  // level_check();
+
+  ___REGION_START __RT_High "0"
+
+  if (level_1) { // sleeping_time x 1
+    nsleep();
+    if (level_2) { // sleeping_time x 2
+      nsleep();
+      if (level_3) { // sleeping_time x 3
+        nsleep();
+        if (level_4) { // sleeping_time x 4
+          nsleep();
+        }
+      }
+    }
+  }
+
+	asm("nop"); // TODO (se-passau/VaRA#296): Workaround for false region detection
+
+  ___REGION_END __RT_High "0"
+
+  return 0;
+}
---
+++ examples/switch.cpp
@@ -0,0 +1,36 @@
+#include "options.h"
+
+int condition = 1;
+long int sleeping_time_seconds = TIME_DURATION_SECONDS;
+long int sleeping_time_nanoseconds = TIME_DURATION_NANOSECONDS;
+
+int main() {
+
+  ___REGION_START __RT_High "0"
+
+  switch (condition) {
+
+    ___REGION_START __RT_High "1"
+  case 1:
+    nsleep();
+    break;
+    ___REGION_END __RT_High "1"
+
+    ___REGION_START __RT_High "2"
+  case 2:
+    nsleep();
+    break;
+    ___REGION_END __RT_High "2"
+
+    ___REGION_START __RT_High "3"
+  default:
+    sleeping_time_seconds = 2;
+    sleeping_time_nanoseconds = 0;
+    nsleep();
+  }
+  ___REGION_END __RT_High "3"
+
+  ___REGION_END __RT_High "0"
+
+  return 0;
+}
